92f8f4e1c620a62f7d0edf5d4ce5fc9f5361dfab
William R Peters <31421828+peterswi@users.noreply.github.com>
2021-02-18 13:37:28 -0500

    Update scrape_practice.py

--- scrape_practice.py
+++ scrape_practice.py
@@ -7,5 +7,17 @@ url="https://www.strava.com/running_races/2"
 html_content = requests.get(url).text
 
 # Parse the html content
-soup = BeautifulSoup(html_content, "html")
-print(soup.prettify()) # print the parsed data of html
+soup = BeautifulSoup(html_content, "html5lib")
+#print(soup.prettify()) # print the parsed data of html
+print(soup.title)
+
+results_table=soup.find("table",attrs={"id":"results_table"})
+print("table:",results_table)
+results_table_data=results_table.tbody.find_all("tr")
+
+headings = []
+for td in results_table_data[0].find_all("th"):
+    # remove any newlines and extra spaces from left and right
+    headings.append(td.b.text.replace('\n', ' ').strip())
+
+print(headings)
