e7f04dc99984960775257b64115737881ecf3238
William R Peters <31421828+peterswi@users.noreply.github.com>
2021-02-18 09:36:31 -0500

    trying to make sense of this

--- strava_scrape.py
+++ strava_scrape.py
@@ -11,3 +11,20 @@ from bs4 import BeautifulSoup
 import csv
 import pandas as pd
 
+# generic strava browse url and makelinks url ?? 
+#Unclear purpose of makelinks rn
+browse_url = "https://www.strava.com/running_races"
+makelinks_url = "https://www.strava.com/running_races/makelinks.cfm"
+
+post_data = {
+    "RaceRange" : "",
+    "MIDD" : "",
+    "SubmitButton" : "View"
+}
+headers = {
+    "Referer" : "",
+    "User-Agent" : ("Mozilla/5.0 (Windows NT 6.1; WOW64) "
+                    "AppleWebKit/537.36 (KHTML, like Gecko) "
+                    "Chrome/34.0.1847.116 Safari/537.36")
+}
+
