3a9e768261b9234a8b7a018fb8fb518043d95110
William R Peters <31421828+peterswi@users.noreply.github.com>
2021-03-13 08:27:22 -0500

    tried some paralelle processing... didnt work

--- processing.py
+++ processing.py
@@ -3,10 +3,9 @@ from passwords import user_pass
 from scrape_strava_activities import scrape_activities
 
 # need an input CSV for each of these
-inputs=[[user_pass[0][0],user_pass[0][1],'inputCsv/input2.csv','outputActivityCsv/output2.csv'],
-[user_pass[1][0],user_pass[1][1],'inputCsv/input3.csv','outputActivityCsv/output3.csv'],
-[user_pass[2][0],user_pass[2][1],'inputCsv/input4.csv','outputActivityCsv/output4.csv'],
-[user_pass[3][0],user_pass[3][1],'inputCsv/input5.csv','outputActivityCsv/output5.csv']]
+inputs=[[user_pass[1][0],user_pass[1][1],'inputCsv/input5.csv','outputActivityCsv/output5.csv'],
+[user_pass[2][0],user_pass[2][1],'inputCsv/input6.csv','outputActivityCsv/output6.csv'],
+[user_pass[3][0],user_pass[3][1],'inputCsv/input7.csv','outputActivityCsv/output7.csv']]
 
-pool=Pool(processes=4)
+pool=Pool(processes=3)
 pool.map(scrape_activities, inputs)
