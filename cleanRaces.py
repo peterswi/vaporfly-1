b4b80021cbd93a6c1ae6d3a0130ae6cce03fea99
William R Peters <31421828+peterswi@users.noreply.github.com>
2021-03-16 12:35:35 -0400

    a bit more data cleaning

--- cleanRaces.py
+++ cleanRaces.py
@@ -4,6 +4,7 @@ import pandas as pd
 def clean(csv):
     data=pd.read_csv(csv)
     data['race']=''
+    data['timeSec']=0
     
     for i in range(25451):
         full=data['race_name'][i]
@@ -24,7 +25,44 @@ def clean(csv):
             data['race'][i]='CHICAGO'
         else:
             data['race']='BOSTON'
+
+        # Rank formatting
+        rank=data['Rank'][i]
+        rank=rank[2:(len(rank)-1)]
+        data['Rank'][i]=rank
+
+        # Name formatting
+        name=data['Name'][i]
+        name=name[2:(len(name)-1)]
+        data['Name'][i]=name
         
-    data.to_csv(csv,index=False)
+        # Gender formatting
+        gender=data['Gender'][i]
+        gender=gender[2:(len(gender)-1)]
+        data['Gender'][i]=gender
+
+        # Age formatting
+        age=data['Age'][i]
+        age=age[2:(len(age)-1)]
+        data['Age'][i]=age
+
+        # Finish formatting
+        finish=data['Finish'][i]
+        finish=finish[2:(len(finish)-1)]
+        data['Finish'][i]=finish
+        if(len(finish)>=7):    
+            hours= int(finish[0])*60*60
+            minu=int(finish[2:4])*60
+            sec=int(finish[5:])
+            data['timeSec'][i]=hours+minu+sec
+        
+        # Finish formatting
+        pace=data['Pace'][i]
+        pace=pace[2:(len(pace)-1)]
+        data['Pace'][i]=pace
+
+        
+    
+    data.to_csv('csv/CleanResults.csv',index=False)
 
 clean('csv/repeatAthletes.csv')
